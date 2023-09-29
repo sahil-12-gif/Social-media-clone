441774bcd01ef3da167e33a23c21255fe3b859bb
sahil-12-gif <130469366+sahil-12-gif@users.noreply.github.com>
2023-09-29 17:14:34 +0530

    add the funcnality of add_user

--- app/models.py
+++ app/models.py
@@ -1,10 +1,31 @@
+# from datetime import datetime
+# from app import db
+
+# class Post(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(140), nullable=False)
+#     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+#     def __repr__(self):
+#         return f"Post('{self.content}', '{self.date_posted}')"
+
+
 from datetime import datetime
 from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(140), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.content}', '{self.date_posted}')"
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,51 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db
-from app.models import Post
-
+# from app.models import Post
+from app.models import Post,User
 # Define the Post model (in models.py, explained below)
 
 @app.route('/')
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        user = User(username=username)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('feed'))
+    return render_template('add_user.html')
+
+@app.route('/user_posts/<int:user_id>')
+def user_posts(user_id):
+    user = User.query.get(user_id)
+    posts = user.posts
+    return render_template('user_posts.html', user=user, posts=posts)
 @app.route('/feed')
 def feed():
     posts = Post.query.all()
     return render_template('feed.html', posts=posts)
 
+
 @app.route('/create_post', methods=['POST'])
 def create_post():
-    post_content = request.form['post_content']
-    post = Post(content=post_content)
-    db.session.add(post)
-    db.session.commit()
-    return redirect(url_for('feed'))
-
+    if request.method == 'POST':
+        print( request.method)
+        content = request.form['content']
+        user_id = request.form['user_id']  # Get user_id from the form
+        # Check if the user with the given user_id exists
+        print(user_id,content)
+        user = User.query.get(user_id)
+        print(user)
+        if user:
+            post = Post(content=content, author=user)
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('feed'))
+        else:
+            # Handle the case where the user does not exist.
+            flash('User not found.', 'danger')  # You can use Flask's flash messages here.
+            return redirect(url_for('feed'))
+      
 @app.route('/edit_post/<int:post_id>', methods=['GET', 'POST'])
 def edit_post(post_id):
     post = Post.query.get(post_id)
@@ -33,3 +61,4 @@ def delete_post(post_id):
     db.session.delete(post)
     db.session.commit()
     return redirect(url_for('feed'))
+
